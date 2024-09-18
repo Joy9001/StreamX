ff99eeab9a62a9962d2448a66fd34ea16a691cff
Garry <priyanshu.k22@iiits.in>
2024-09-18 15:06:20 +0530

    Update routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,28 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Home from "./components/Home.jsx";
+// import Login from "./components/Login.jsx";
+// import SignUp from "./components/SignUp.jsx";
 
-export function App() {
-
+function App() {
+  const router = createBrowserRouter([
+    // {
+    //   path: "/",
+    //   element: <Home />,
+    // },
+    // {
+    //   path: "/login",
+    //   element: <Login />,
+    // },
+    // {
+    //   path: "/signup",
+    //   element: <SignUp />,
+    // },
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
+
+export default App;
