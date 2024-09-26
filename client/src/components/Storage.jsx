2cd6ee00c2f0cfdd927e7676c9b24e03330e6a57
Joy Mridha <joymridha939@gmail.com>
2024-09-26 15:54:43 +0530

    Connect storage backend and frontend

--- client/src/components/Storage.jsx
+++ client/src/components/Storage.jsx
@@ -1,7 +1,9 @@
-import { useRef } from 'react'
-import { useRecoilValue } from 'recoil'
+import axios from 'axios'
+import { useEffect, useRef, useState } from 'react'
+import { useRecoilState, useRecoilValue } from 'recoil'
 import { drawerState } from '../states/drawerState.js'
 import { navbarOpenState } from '../states/navbarState.js'
+import { allVidState, recentVidState } from '../states/videoState.js'
 import ContentTable from './ContentTable'
 import Navbar from './Navbar.jsx'
 import RecentCard from './RecentCard'
@@ -12,10 +14,61 @@ function Storage() {
   const drawer = useRecoilValue(drawerState)
   const navOpen = useRecoilValue(navbarOpenState)
   const fileInputRef = useRef(null)
+  const [uploading, setUploading] = useState(false)
+  const [recentVideos, setRecentVideos] = useRecoilState(recentVidState)
+  const setAllVideos = useRecoilState(allVidState)[1]
 
   function handleNewBtnClick() {
     fileInputRef.current.click()
   }
+
+  async function handleFileUpload(e) {
+    setUploading(true)
+    const file = e.target.files[0]
+
+    if (file) {
+      const formData = new FormData()
+      formData.append('file', file)
+
+      try {
+        const res = await axios.post(
+          'http://localhost:3000/api/videos/upload',
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+            },
+          }
+        )
+
+        console.log(res)
+        setUploading(false)
+        setRecentVideos((prev) => [res.data.savedVideo, ...prev])
+        setAllVideos((prev) => [res.data.savedVideo, ...prev])
+        alert(res.data.message)
+      } catch (error) {
+        console.error('Error uploading file:', error)
+        alert('File upload failed!')
+      } finally {
+        e.target.value = ''
+      }
+    }
+  }
+
+  // Fetch recent videos
+  useEffect(() => {
+    async function fetchRecentVideos() {
+      try {
+        const res = await axios.get('http://localhost:3000/api/videos/recent')
+        console.log(res.data)
+        setRecentVideos(res.data.videos)
+      } catch (error) {
+        console.error('Error fetching recent videos:', error)
+      }
+    }
+
+    fetchRecentVideos()
+  }, [setRecentVideos])
 
   return (
     <div className='storage-main flex h-screen'>
@@ -46,45 +99,50 @@ function Storage() {
                 </div>
                 <div className='storage-new-btn'>
                   <button
-                    className='btn btn-secondary'
+                    className={`btn ${uploading ? 'btn-disabled' : 'btn-secondary'}`}
                     onClick={handleNewBtnClick}>
-                    <svg
-                      className='h-6 w-6'
-                      viewBox='0 0 24 24'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'>
-                      <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
-                      <g
-                        id='SVGRepo_tracerCarrier'
-                        strokeLinecap='round'
-                        strokeLinejoin='round'></g>
-                      <g id='SVGRepo_iconCarrier'>
-                        {' '}
-                        <path
-                          d='M6 12H18M12 6V18'
-                          stroke='#000000'
-                          strokeWidth='2'
+                    {uploading ? (
+                      <span className='loading loading-spinner loading-sm'></span>
+                    ) : (
+                      <svg
+                        className='h-6 w-6'
+                        viewBox='0 0 24 24'
+                        fill='none'
+                        xmlns='http://www.w3.org/2000/svg'>
+                        <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
+                        <g
+                          id='SVGRepo_tracerCarrier'
                           strokeLinecap='round'
-                          strokeLinejoin='round'></path>{' '}
-                      </g>
-                    </svg>
-                    New
+                          strokeLinejoin='round'></g>
+                        <g id='SVGRepo_iconCarrier'>
+                          {' '}
+                          <path
+                            d='M6 12H18M12 6V18'
+                            stroke='#000000'
+                            strokeWidth='2'
+                            strokeLinecap='round'
+                            strokeLinejoin='round'></path>{' '}
+                        </g>
+                      </svg>
+                    )}
+
+                    {uploading ? 'Uploading...' : 'New'}
                   </button>
                   <input
                     type='file'
                     accept='video/*'
                     ref={fileInputRef}
-                    onChange={(e) => console.log(e.target.files)}
+                    onChange={handleFileUpload}
                     className='hidden'
+                    disabled={uploading}
                   />
                 </div>
               </div>
 
               <div className='storage-recent-body no-scrollbar m-2 flex w-full overflow-x-scroll'>
-                <RecentCard />
-                <RecentCard />
-                <RecentCard />
-                <RecentCard />
+                {recentVideos.map((video) => (
+                  <RecentCard key={video._id} video={video} />
+                ))}
               </div>
             </div>
             <div className='storage-content mt-2'>
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import express from 'express'
 import morgan from 'morgan'
 import connectMongo from './db/connectMongo.db.js'
+import VideoRouter from './routes/video.route.js'
 
 dotenv.config()
 
@@ -21,8 +22,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.send('Backend is up!')
 })
+
+app.use('/api/videos', VideoRouter)
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`)
