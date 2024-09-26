8bcd5d029c1cf4f14fd6c9389bd836a49d785ede
Joy Mridha <joymridha939@gmail.com>
2024-09-26 10:49:25 +0530

    Remove profile from storage

--- client/src/components/StorageNav.jsx
+++ client/src/components/StorageNav.jsx
@@ -15,7 +15,7 @@ function StorageNav() {
       </div>
       <div className='flex flex-none items-center gap-2'>
         <div className='form-control'>
-          <label className='input input-sm input-bordered input-primary flex items-center gap-2'>
+          <label className='input input-md input-bordered input-primary flex w-60 items-center gap-2'>
             <svg
               xmlns='http://www.w3.org/2000/svg'
               viewBox='0 0 16 16'
@@ -29,32 +29,6 @@ function StorageNav() {
             </svg>
             <input type='text' className='grow' placeholder='Search' />
           </label>
-        </div>
-        <div className='dropdown dropdown-end'>
-          <div
-            tabIndex={0}
-            role='button'
-            className='avatar btn btn-circle btn-ghost'>
-            <div className='w-10 rounded-full'>
-              <img
-                alt='Tailwind CSS Navbar component'
-                src='https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp'
-              />
-            </div>
-          </div>
-          <ul
-            tabIndex={0}
-            className='menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow'>
-            <li>
-              <a className='justify-between'>Profile</a>
-            </li>
-            <li>
-              <a>Settings</a>
-            </li>
-            <li>
-              <a>Logout</a>
-            </li>
-          </ul>
         </div>
       </div>
     </div>
