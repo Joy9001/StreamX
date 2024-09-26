0f19243253b7183c9c8e5fe5567027a65894ecb0
Rish2003 <rishabhraj6421@gmail.com>
2024-09-27 03:10:44 +0530

    form frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,13 @@
 // import Login from "./components/Login.jsx";
 // import SignUp from "./components/SignUp.jsx";
 //import Navbar from './components/Navbar.jsx'
-import Profile from './components/Profile.jsx'
+//import Profile from './components/Profile.jsx'
 //import Header from './components/Header.jsx'
 //import UserProfile from './components/UserProfile.jsx'
 //import HiredEditors from './components/HiredEditors.jsx'
 // import Radial from './components/Radial.jsx'
 //import RecentCard from './components/RecentCard.jsx'
+import ProfileForm from './components/ProfileForm'
 
 function App() {
   //const router = createBrowserRouter([
@@ -34,7 +35,7 @@ function App() {
   //])
   return (
     <>
-      <Profile />
+      <ProfileForm />
     </>
   )
 }
