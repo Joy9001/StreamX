d38b6a71e11ff4adc80d2cfffd18ee756ae2d9f2
Joy Mridha <joymridha939@gmail.com>
2024-09-26 11:05:57 +0530

    Setup express in server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,32 @@
-import connectMongo from './db/connectMongo.db.js'
+import cors from 'cors'
 import dotenv from 'dotenv'
+import express from 'express'
+import morgan from 'morgan'
+import connectMongo from './db/connectMongo.db.js'
 
 dotenv.config()
 
-connectMongo().then(() => {
-	console.log('Connected to MongoDB')
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(
+	cors({
+		origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+		credentidals: true,
+	})
+)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
+	connectMongo().then(() => {
+		console.log('MongoDB connected')
+	})
 })
