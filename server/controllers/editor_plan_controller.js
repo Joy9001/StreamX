d3913f5673dce370dd9756a2b7f1d125bde04ed1
Joy Mridha <joymridha939@gmail.com>
2024-10-11 09:49:33 +0530

    Remove 'model' files to 'models' folder

--- server/controllers/editor_plan_controller.js
+++ server/controllers/editor_plan_controller.js
@@ -1,37 +1,36 @@
-import editor_plans from "../model/Editor_gig_plans.js";
-import Editor_Gig from "../model/Editor_gig.js";
+import Editor_Gig from '../models/Editor_gig.js'
+import editor_plans from '../models/Editor_gig_plans.js'
 
 export const Editor_gig_plans = async (req, res) => {
-    try {
-        const { email, basic, standard, premium } = req.body;
+	try {
+		const { email, basic, standard, premium } = req.body
 
-        // Check if the editor with the provided email exists
-        const existingEditor = await Editor_Gig.findOne({ email });
-        if (!existingEditor) {
-            return res.status(404).json({
-                success: false,
-                message: 'No editor found with this email.',
-            });
-        }
+		// Check if the editor with the provided email exists
+		const existingEditor = await Editor_Gig.findOne({ email })
+		if (!existingEditor) {
+			return res.status(404).json({
+				success: false,
+				message: 'No editor found with this email.',
+			})
+		}
 
-        const updatedPlan = await editor_plans.findOneAndUpdate(
-            { email }, 
-            { basic, standard, premium }, // Update
-            { new: true, upsert: true } 
-        );
+		const updatedPlan = await editor_plans.findOneAndUpdate(
+			{ email },
+			{ basic, standard, premium }, // Update
+			{ new: true, upsert: true }
+		)
 
-        res.status(201).json({
-            success: true,
-            data: updatedPlan,
-            message: 'Editor gig plan updated/created successfully!',
-        });
-    } catch (error) {
-        console.error('Error updating/creating editor gig plan:', error);
-        res.status(500).json({
-            success: false,
-            message: 'An error occurred while updating/creating the editor gig plan.',
-            error: error.message,
-        });
-    }
-};
-
+		res.status(201).json({
+			success: true,
+			data: updatedPlan,
+			message: 'Editor gig plan updated/created successfully!',
+		})
+	} catch (error) {
+		console.error('Error updating/creating editor gig plan:', error)
+		res.status(500).json({
+			success: false,
+			message: 'An error occurred while updating/creating the editor gig plan.',
+			error: error.message,
+		})
+	}
+}
