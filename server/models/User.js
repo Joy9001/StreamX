7900140f96949b7c30b76ba53d05a0f4ed549e48
Joy Mridha <joymridha939@gmail.com>
2024-12-02 03:28:12 +0530

    Refactor MongoDB model files to use consistent import syntax and improve code readability

--- server/models/User.js
+++ server/models/User.js
@@ -1,43 +1,46 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ['superuser', 'admin', 'testuser'],
-  },
-  Id: {
-    type: String,
-    required: true,
-  },
-  permissions: {
-    owner: {
-      edit: { type: Boolean, default: false },
-      delete: { type: Boolean, default: false },
-    },
-    editor: {
-      edit: { type: Boolean, default: false },
-      delete: { type: Boolean, default: false },
-    },
-    editorGig: {
-      edit: { type: Boolean, default: false },
-      delete: { type: Boolean, default: false },
-    },
-    video: {
-      delete: { type: Boolean, default: false },
-    },
-  },
-}, {
-  timestamps: true,
-});
+const userSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		role: {
+			type: String,
+			enum: ['superuser', 'admin', 'testuser'],
+		},
+		Id: {
+			type: String,
+			required: true,
+		},
+		permissions: {
+			owner: {
+				edit: { type: Boolean, default: false },
+				delete: { type: Boolean, default: false },
+			},
+			editor: {
+				edit: { type: Boolean, default: false },
+				delete: { type: Boolean, default: false },
+			},
+			editorGig: {
+				edit: { type: Boolean, default: false },
+				delete: { type: Boolean, default: false },
+			},
+			video: {
+				delete: { type: Boolean, default: false },
+			},
+		},
+	},
+	{
+		timestamps: true,
+	}
+)
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
 
-export default User;
+export default User
