1567184c4bcc55871cbe1ceb88e4678dfdb770a8
Joy Mridha <joymridha939@gmail.com>
2024-12-04 01:33:26 +0530

    Add request upload button to UploadFooter component

--- client/src/components/Storage/YtFormComponents/UploadFooter.jsx
+++ client/src/components/Storage/YtFormComponents/UploadFooter.jsx
@@ -1,18 +1,27 @@
 import PropTypes from 'prop-types'
 import YtUploadBtn from '../YtStatusBtnComponents/YtUploadBtn'
 
-const UploadFooter = ({ onUpload }) => {
+const UploadFooter = ({ onUpload, onRequest }) => {
   return (
     <footer className='mt-4 flex items-center justify-between border-t border-neutral-600 pt-4'>
-      <div onClick={onUpload}>
-        <YtUploadBtn />
+      <div className='flex gap-4'>
+        <div onClick={onUpload}>
+          <YtUploadBtn />
+        </div>
+        <button
+          onClick={onRequest}
+          className='btn btn-outline btn-neutral'
+          type='button'>
+          <span>Request Upload</span>
+        </button>
       </div>
     </footer>
   )
 }
 
 UploadFooter.propTypes = {
-  onUpload: PropTypes.func.isRequired, // Function to call when upload button is clicked
+  onUpload: PropTypes.func.isRequired,
+  onRequest: PropTypes.func.isRequired,
 }
 
 export default UploadFooter
