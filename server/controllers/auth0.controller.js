17f654b215220b24a12db43bba07d83491621b6a
Joy Mridha <joymridha939@gmail.com>
2024-12-04 01:39:12 +0530

    Enhance auth0CreateController to manage Google OAuth tokens for Owners and add handling for Admin role creation

--- server/controllers/auth0.controller.js
+++ server/controllers/auth0.controller.js
@@ -11,18 +11,38 @@ export const auth0CreateController = async (req, res) => {
 		console.log('/auth0/create', user)
 
 		if (user.user_metadata.role === 'Owner') {
+			// Owner
 			const findOwner = await Owner.findOne({ email: user.email })
 			const providerSub = user.identities.map((identity) => `${identity.provider}|${identity.user_id}`)
+
+			let googleIdentity = user.identities.find((identity) => identity.provider === 'google-oauth2')
+			let gAccessToken = ''
+			let gRefreshToken = ''
+			if (googleIdentity) {
+				gAccessToken = googleIdentity.access_token
+				gRefreshToken = googleIdentity.refresh_token
+			}
+
 			if (findOwner) {
+				// Owner already exists
 				let message = 'Owner already exists'
 				if (findOwner.providerSub !== providerSub) {
-					await Owner.findOneAndUpdate({ email: user.email }, { providerSub: providerSub })
+					await Owner.findOneAndUpdate(
+						{ email: user.email },
+						{
+							providerSub: providerSub,
+							gTokens: {
+								accessToken: gAccessToken,
+								refreshToken: gRefreshToken,
+							},
+						}
+					)
 					message = 'Owner Updated'
 				}
 				console.log(message)
-
 				return res.status(200).json({ message, user: { ...user, _id: findOwner._id } })
 			}
+
 			const owner = new Owner({
 				username: user.nickname,
 				email: user.email,
@@ -39,9 +59,12 @@ export const auth0CreateController = async (req, res) => {
 				},
 			})
 		} else if (user.user_metadata.role === 'Editor') {
+			// Editor
 			const findEditor = await Editor.findOne({ email: user.email })
 			const providerSub = user.identities.map((identity) => `${identity.provider}|${identity.user_id}`)
+
 			if (findEditor) {
+				// Editor already exists
 				let message = 'Editor already exists'
 				if (findEditor.providerSub !== providerSub) {
 					await Editor.findOneAndUpdate({ email: user.email }, { providerSub: providerSub })
@@ -67,9 +90,12 @@ export const auth0CreateController = async (req, res) => {
 			console.log('Editor created successfully')
 			return res.status(200).json({ message: 'Editor created successfully', user: { ...user, _id: editor._id } })
 		} else if (user.user_metadata.role === 'Admin') {
+			// Admin
 			const findAdmin = await Admin.findOne({ email: user.email })
 			const providerSub = user.identities.map((identity) => `${identity.provider}|${identity.user_id}`)
+
 			if (findAdmin) {
+				// Admin already exists
 				let message = 'Admin already exists'
 				if (findAdmin.providerSub !== providerSub) {
 					await Admin.findOneAndUpdate({ email: user.email }, { providerSub: providerSub })
@@ -85,6 +111,7 @@ export const auth0CreateController = async (req, res) => {
 					},
 				})
 			}
+
 			const admin = new Admin({
 				username: user.nickname,
 				email: user.email,
