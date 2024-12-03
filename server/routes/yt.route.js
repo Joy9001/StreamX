aa4f0c6235fe83f18fa7a79ac7fbdb4e5a21a5b1
Joy Mridha <joymridha939@gmail.com>
2024-12-03 16:57:22 +0530

    Update upload route to include role and userId parameters

--- server/routes/yt.route.js
+++ server/routes/yt.route.js
@@ -2,6 +2,6 @@ import { Router } from 'express'
 import { uploadController } from '../controllers/yt.controller.js'
 const router = Router()
 
-router.post('/upload', uploadController)
+router.post('/upload/:role/:userId', uploadController)
 
 export default router
