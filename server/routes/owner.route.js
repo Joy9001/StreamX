898f338c486f7319b17d19b429d603f506d629ac
Joy Mridha <joymridha939@gmail.com>
2024-12-04 03:39:09 +0530

    Add route to fetch hired editors for a specific owner in owner.route.js

--- server/routes/owner.route.js
+++ server/routes/owner.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { 
-  getOwnerProfile, 
-  getAllOwners, 
-  createOwner, 
-  deleteOwner, 
-  updateOwner,
-  getOwnerByEmail 
+import {
+	createOwner,
+	deleteOwner,
+	getAllOwners,
+	getHiredEditors,
+	getOwnerByEmail,
+	getOwnerProfile,
+	updateOwner,
 } from '../controllers/owner.controller.js'
 import { createOwnerProfile, updateOwnerProfile } from '../controllers/profileSetting.controller.js'
 import { upload } from '../middlewares/multer.middleware.js'
@@ -14,7 +15,7 @@ const router = express.Router()
 
 // Admin dashboard routes
 router.get('/ownerProfile', getAllOwners)
-router.get('/ownerProfile/:email', getOwnerByEmail)  
+router.get('/ownerProfile/:email', getOwnerByEmail)
 router.post('/ownerProfile', createOwner)
 router.patch('/ownerProfile/:email', updateOwner)
 router.delete('/ownerProfile/:email', deleteOwner)
@@ -24,4 +25,5 @@ router.get('/owner/profile/:id', getOwnerProfile)
 router.post('/owner/profile/setup/:id', upload.single('file'), createOwnerProfile)
 router.put('/owner/profile/settings/:id', upload.single('file'), updateOwnerProfile)
 
+router.get('/hired-editors/:ownerId', getHiredEditors)
 export default router
