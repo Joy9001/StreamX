cc9d5581ab9e4f8fae25d52fbc78aed3de0e2036
Joy Mridha <joymridha939@gmail.com>
2024-12-04 03:57:06 +0530

    Remove required validation for ownerId in video model schema

--- server/models/video.model.js
+++ server/models/video.model.js
@@ -4,7 +4,7 @@ const videoSchema = new Schema(
 	{
 		ownerId: {
 			type: Schema.Types.ObjectId,
-			required: [true, 'Owner Id is required'],
+			// required: [true, 'Owner Id is required'],
 			ref: 'User',
 		},
 		editorId: {
