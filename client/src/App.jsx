8001d60f216ca4968a840ff7cfa5880ebb54bd84
Joy Mridha <joymridha939@gmail.com>
2024-12-04 07:22:58 +0530

    Refactor routing in App component to conditionally navigate users based on authentication and admin role

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,10 @@
 import { useAuth0 } from '@auth0/auth0-react'
+import { useSelector } from 'react-redux'
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import { Dashboard as EditorDashboard } from './components/AdminPanel/AdminEditorDashboard.jsx'
+import { Dashboard as OwnerDashboard } from './components/AdminPanel/AdminOwnerDashboard.jsx'
 import AdminPanel from './components/AdminPanel/AdminPanel.jsx'
+import { Dashboard as VideosDashboard } from './components/AdminPanel/AdminVideosDashboard.jsx'
 import EditorUi from './components/EditorProfile/EditorUi.jsx'
 import GigProfile from './components/GigProfile/GigProfile.jsx'
 import HiredEditor from './components/HiredEditor/HiredEditor.jsx'
@@ -8,46 +12,155 @@ import Landing from './components/LandingPage/Landing.jsx'
 import Profile from './components/OwnerProfile/Profile.jsx'
 import RequestApprove from './components/Request&Apporved/raas.jsx'
 import Storage from './components/Storage/Storage.jsx'
-
 function App() {
   const { isAuthenticated } = useAuth0()
+  const userData = useSelector((state) => state.user.userData)
+  const isAdmin = userData?.user_metadata?.role === 'Admin'
 
   const router = createBrowserRouter([
     {
       path: '/',
       element: isAuthenticated ? (
-        <Navigate to='/storage' replace />
+        isAdmin ? (
+          <Navigate to='/admin-panel' replace />
+        ) : (
+          <Navigate to='/storage' replace />
+        )
       ) : (
         <Landing />
       ),
     },
     {
       path: '/profile/owner',
-      element: isAuthenticated ? <Profile /> : <Landing />,
+      element: isAuthenticated ? (
+        isAdmin ? (
+          <Navigate to='/admin-panel' replace />
+        ) : (
+          <Profile />
+        )
+      ) : (
+        <Landing />
+      ),
     },
     {
       path: '/profile/editor',
-      element: isAuthenticated ? <EditorUi /> : <Landing />,
+      element: isAuthenticated ? (
+        isAdmin ? (
+          <Navigate to='/admin-panel' replace />
+        ) : (
+          <EditorUi />
+        )
+      ) : (
+        <Landing />
+      ),
     },
     {
       path: '/gig-profile',
-      element: isAuthenticated ? <GigProfile /> : <Landing />,
+      element: isAuthenticated ? (
+        isAdmin ? (
+          <Navigate to='/admin-panel' replace />
+        ) : (
+          <GigProfile />
+        )
+      ) : (
+        <Landing />
+      ),
     },
     {
       path: '/storage',
-      element: isAuthenticated ? <Storage /> : <Landing />,
+      element: isAuthenticated ? (
+        isAdmin ? (
+          <Navigate to='/admin-panel' replace />
+        ) : (
+          <Storage />
+        )
+      ) : (
+        <Landing />
+      ),
     },
     {
       path: '/req-n-approve',
-      element: isAuthenticated ? <RequestApprove /> : <Landing />,
+      element: isAuthenticated ? (
+        isAdmin ? (
+          <Navigate to='/admin-panel' replace />
+        ) : (
+          <RequestApprove />
+        )
+      ) : (
+        <Landing />
+      ),
     },
     {
       path: '/hire-editors',
-      element: isAuthenticated ? <HiredEditor /> : <Landing />,
+      element: isAuthenticated ? (
+        isAdmin ? (
+          <Navigate to='/admin-panel' replace />
+        ) : (
+          <HiredEditor />
+        )
+      ) : (
+        <Landing />
+      ),
     },
     {
       path: '/admin-panel',
-      element: isAuthenticated ? <AdminPanel /> : <Landing />,
+      element: isAuthenticated ? (
+        isAdmin ? (
+          <AdminPanel />
+        ) : (
+          <Landing />
+        )
+      ) : (
+        <Landing />
+      ),
+    },
+    {
+      path: '/admin-panel/owners',
+      element: isAuthenticated ? (
+        isAdmin ? (
+          <OwnerDashboard />
+        ) : (
+          <Landing />
+        )
+      ) : (
+        <Landing />
+      ),
+    },
+    {
+      path: '/admin-panel/editors',
+      element: isAuthenticated ? (
+        isAdmin ? (
+          <EditorDashboard />
+        ) : (
+          <Landing />
+        )
+      ) : (
+        <Landing />
+      ),
+    },
+    {
+      path: '/admin-panel/videos',
+      element: isAuthenticated ? (
+        isAdmin ? (
+          <VideosDashboard />
+        ) : (
+          <Landing />
+        )
+      ) : (
+        <Landing />
+      ),
+    },
+    {
+      path: '/admin-panel/requests',
+      element: isAuthenticated ? (
+        isAdmin ? (
+          <EditorDashboard />
+        ) : (
+          <Landing />
+        )
+      ) : (
+        <Landing />
+      ),
     },
   ])
   return (
