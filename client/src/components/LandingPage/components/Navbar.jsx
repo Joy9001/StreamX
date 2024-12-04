0ef3f6c7f6f35b0f4a77916a6bb23d2fb774ab6a
Joy Mridha <joymridha939@gmail.com>
2024-12-04 05:39:14 +0530

    Add Auth0 login functionality to Navbar and Headline components

--- client/src/components/LandingPage/components/Navbar.jsx
+++ client/src/components/LandingPage/components/Navbar.jsx
@@ -1,8 +1,11 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import { useContext, useState } from 'react'
 import { ActiveTabContext } from '../contexts/ActiveTabContext'
 
-export default () => {
+export default function Navbar() {
   const { activeTab, setActiveTab } = useContext(ActiveTabContext)
+  const { loginWithRedirect } = useAuth0()
+
   const [open, setOpen] = useState(false)
   const onClickHandler = (tabClicked) => {
     setOpen(!open)
@@ -11,12 +14,12 @@ export default () => {
   return (
     <nav
       className={
-        'fixed inset-x-0 top-0 z-50 grid auto-rows-min grid-cols-2 items-center justify-between bg-white px-5 py-6 drop-shadow-md md:px-[100px] lg:bottom-auto lg:flex lg:flex-row lg:py-2 h-20' +
+        'fixed inset-x-0 top-0 z-50 grid h-20 auto-rows-min grid-cols-2 items-center justify-between bg-white px-5 py-6 drop-shadow-md md:px-[100px] lg:bottom-auto lg:flex lg:flex-row lg:py-2' +
         (open ? ' bottom-0' : ' ')
       }>
       <div className='flex'>
         <img
-          className=' h-[3rem] w-[3rem]'
+          className='h-[3rem] w-[3rem]'
           src='http://localhost:5173/src/assets/logoX.png'
           alt='logo'
         />
@@ -29,7 +32,7 @@ export default () => {
         }>
         <li
           className={
-            'hover:bg-blue py-4 text-center duration-300 hover:bg-opacity-10 lg:hover:bg-white lg:hover:bg-opacity-100 lg:hover:font-semibold text-[18px]' +
+            'hover:bg-blue py-4 text-center text-[18px] duration-300 hover:bg-opacity-10 lg:hover:bg-white lg:hover:bg-opacity-100 lg:hover:font-semibold' +
             (activeTab == 'home' ? ' lg:font-semibold' : '')
           }
           onClick={() => onClickHandler('home')}>
@@ -37,7 +40,7 @@ export default () => {
         </li>
         <li
           className={
-            'hover:bg-blue py-4 text-center duration-300 hover:bg-opacity-10 lg:hover:bg-white lg:hover:bg-opacity-100 lg:hover:font-semibold text-[18px]' +
+            'hover:bg-blue py-4 text-center text-[18px] duration-300 hover:bg-opacity-10 lg:hover:bg-white lg:hover:bg-opacity-100 lg:hover:font-semibold' +
             (activeTab == 'about' ? ' lg:font-semibold' : '')
           }
           onClick={() => onClickHandler('about')}>
@@ -45,7 +48,7 @@ export default () => {
         </li>
         <li
           className={
-            'hover:bg-blue py-4 text-center duration-300 hover:bg-opacity-10 lg:hover:bg-white lg:hover:bg-opacity-100 lg:hover:font-semibold text-[18px]' +
+            'hover:bg-blue py-4 text-center text-[18px] duration-300 hover:bg-opacity-10 lg:hover:bg-white lg:hover:bg-opacity-100 lg:hover:font-semibold' +
             (activeTab == 'services' ? ' lg:font-semibold' : '')
           }
           onClick={() => onClickHandler('services')}>
@@ -53,7 +56,7 @@ export default () => {
         </li>
         <li
           className={
-            'hover:bg-blue py-4 text-center duration-300 hover:bg-opacity-10 lg:hover:bg-white lg:hover:bg-opacity-100 lg:hover:font-semibold text-[18px]' +
+            'hover:bg-blue py-4 text-center text-[18px] duration-300 hover:bg-opacity-10 lg:hover:bg-white lg:hover:bg-opacity-100 lg:hover:font-semibold' +
             (activeTab == 'projects' ? ' lg:font-semibold' : '')
           }
           onClick={() => onClickHandler('projects')}>
@@ -61,7 +64,7 @@ export default () => {
         </li>
         <li
           className={
-            'hover:bg-blue py-4 text-center duration-300 hover:bg-opacity-10 lg:hover:bg-white lg:hover:bg-opacity-100 lg:hover:font-semibold text-[18px]' +
+            'hover:bg-blue py-4 text-center text-[18px] duration-300 hover:bg-opacity-10 lg:hover:bg-white lg:hover:bg-opacity-100 lg:hover:font-semibold' +
             (activeTab == 'testimonial' ? ' lg:font-semibold' : '')
           }
           onClick={() => onClickHandler('testimonial')}>
@@ -69,11 +72,26 @@ export default () => {
         </li>
         <li
           className={
-            'hover:bg-blue py-4 text-center duration-300 hover:bg-opacity-10 lg:hover:bg-white lg:hover:bg-opacity-100 lg:hover:font-semibold text-[18px]' +
+            'hover:bg-blue py-4 text-center text-[18px] duration-300 hover:bg-opacity-10 lg:hover:bg-white lg:hover:bg-opacity-100 lg:hover:font-semibold' +
             (activeTab == 'testimonial' ? ' lg:font-semibold' : '')
           }>
-          <button className="bg-transparent text-blue-600  font-semibold px-4 rounded-lg">
-            Sign in
+          <button
+            className='rounded-lg bg-transparent px-4 font-semibold text-blue-600'
+            onClick={() =>
+              loginWithRedirect({
+                access_type: 'offline',
+                connection_scope: 'https://www.googleapis.com/auth/youtube',
+                authorizationParams: {
+                  // connection: 'google-oauth2',
+                  access_type: 'offline',
+                  connection_scope: 'https://www.googleapis.com/auth/youtube',
+                  prompt: 'consent',
+                  scope:
+                    'read:users read:user_idp_tokens read:current_user read:current_user_metadata update:current_user_metadata openid profile email offline_access',
+                },
+              })
+            }>
+            Sign In
           </button>
         </li>
       </ul>
--- client/src/components/LandingPage/pages/Home/components/Headline.jsx
+++ client/src/components/LandingPage/pages/Home/components/Headline.jsx
@@ -1,4 +1,7 @@
+import { useAuth0 } from '@auth0/auth0-react'
+
 export default function Headline() {
+  const { loginWithRedirect } = useAuth0()
   return (
     <section
       id='home'
@@ -63,7 +66,21 @@ export default function Headline() {
             transition: 'background-color 0.3s',
           }}
           onMouseOver={(e) => (e.target.style.backgroundColor = '#4338CA')}
-          onMouseOut={(e) => (e.target.style.backgroundColor = '#4F46E5')}>
+          onMouseOut={(e) => (e.target.style.backgroundColor = '#4F46E5')}
+          onClick={() =>
+            loginWithRedirect({
+              access_type: 'offline',
+              connection_scope: 'https://www.googleapis.com/auth/youtube',
+              authorizationParams: {
+                // connection: 'google-oauth2',
+                access_type: 'offline',
+                connection_scope: 'https://www.googleapis.com/auth/youtube',
+                prompt: 'consent',
+                scope:
+                  'read:users read:user_idp_tokens read:current_user read:current_user_metadata update:current_user_metadata openid profile email offline_access',
+              },
+            })
+          }>
           Sign In
         </a>
       </div>
