6c6ca07e33e572ad7e9f076a6263d8fc9107a904
Joy Mridha <joymridha939@gmail.com>
2025-03-13 04:30:46 +0530

    Integrate Auth0 for user authentication in Header component
    - Import `useAuth0` hook for authentication functionalities
    - Add `loginWithRedirect` method from `useAuth0`
    - Implement `handleAuth0Login` to redirect users for Auth0 login
    - Configure Auth0 login with required parameters and scopes
    - Replace "New account" link and "Sign in" button with Auth0 login trigger

--- client/src/components/LandingPage/components/Header.jsx
+++ client/src/components/LandingPage/components/Header.jsx
@@ -1,67 +1,80 @@
-import { useLocation } from "react-router-dom";
-import { disablePageScroll, enablePageScroll } from "scroll-lock";
+import { useAuth0 } from '@auth0/auth0-react'
+import { useLocation } from 'react-router-dom'
+import { disablePageScroll, enablePageScroll } from 'scroll-lock'
 
-import { logoX } from "../assets";
-import { navigation } from "../constants";
-import Button from "./Button";
-import MenuSvg from "../assets/svg/MenuSvg";
-import { HamburgerMenu } from "./design/Header";
-import { useState } from "react";
+import { useState } from 'react'
+import { logoX } from '../assets'
+import MenuSvg from '../assets/svg/MenuSvg'
+import { navigation } from '../constants'
+import Button from './Button'
+import { HamburgerMenu } from './design/Header'
 
 const Header = () => {
-  const pathname = useLocation();
-  const [openNavigation, setOpenNavigation] = useState(false);
+  const pathname = useLocation()
+  const [openNavigation, setOpenNavigation] = useState(false)
+  const { loginWithRedirect } = useAuth0()
 
   const toggleNavigation = () => {
     if (openNavigation) {
-      setOpenNavigation(false);
-      enablePageScroll();
+      setOpenNavigation(false)
+      enablePageScroll()
     } else {
-      setOpenNavigation(true);
-      disablePageScroll();
+      setOpenNavigation(true)
+      disablePageScroll()
     }
-  };
+  }
 
   const handleClick = () => {
-    if (!openNavigation) return;
+    if (!openNavigation) return
 
-    enablePageScroll();
-    setOpenNavigation(false);
-  };
+    enablePageScroll()
+    setOpenNavigation(false)
+  }
+
+  const handleAuth0Login = () => {
+    loginWithRedirect({
+      access_type: 'offline',
+      connection_scope: 'https://www.googleapis.com/auth/youtube',
+      authorizationParams: {
+        access_type: 'offline',
+        connection_scope: 'https://www.googleapis.com/auth/youtube',
+        prompt: 'consent',
+        scope:
+          'read:users read:user_idp_tokens read:current_user read:current_user_metadata update:current_user_metadata openid profile email offline_access',
+      },
+    })
+  }
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
-        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
-      }`}
-    >
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <div className="flex space-x-3">
-            <img src={logoX} width={50} height={40} alt="StreamX" />
-            <p className="py-5 text-gray-400">StreamX</p>
+      className={`fixed left-0 top-0 z-50 w-full border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+        openNavigation ? 'bg-n-8' : 'bg-n-8/90 backdrop-blur-sm'
+      }`}>
+      <div className='flex items-center px-5 max-lg:py-4 lg:px-7.5 xl:px-10'>
+        <a className='block w-[12rem] xl:mr-8' href='#hero'>
+          <div className='flex space-x-3'>
+            <img src={logoX} width={50} height={40} alt='StreamX' />
+            <p className='py-5 text-gray-400'>StreamX</p>
           </div>
         </a>
 
         <nav
           className={`${
-            openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
-        >
-          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+            openNavigation ? 'flex' : 'hidden'
+          } fixed bottom-0 left-0 right-0 top-[5rem] bg-n-8 lg:static lg:mx-auto lg:flex lg:bg-transparent`}>
+          <div className='relative z-2 m-auto flex flex-col items-center justify-center lg:flex-row'>
             {navigation.map((item) => (
               <a
                 key={item.id}
                 href={item.url}
                 onClick={handleClick}
-                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                  item.onlyMobile ? "lg:hidden" : ""
+                className={`relative block font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
+                  item.onlyMobile ? 'lg:hidden' : ''
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
                   item.url === pathname.hash
-                    ? "z-2 lg:text-n-1"
-                    : "lg:text-n-1/50"
-                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
-              >
+                    ? 'z-2 lg:text-n-1'
+                    : 'lg:text-n-1/50'
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}>
                 {item.title}
               </a>
             ))}
@@ -70,26 +83,24 @@ const Header = () => {
           <HamburgerMenu />
         </nav>
 
-        <a
-          href="#signup"
-          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
-        >
+        <div
+          className='button mr-8 hidden text-n-1/50 transition-colors hover:cursor-pointer hover:text-n-1 lg:block'
+          onClick={handleAuth0Login}>
           New account
-        </a>
-        <Button className="hidden lg:flex" href="#login">
+        </div>
+        <Button className='hidden lg:flex' onClick={handleAuth0Login}>
           Sign in
         </Button>
 
         <Button
-          className="ml-auto lg:hidden"
-          px="px-3"
-          onClick={toggleNavigation}
-        >
+          className='ml-auto lg:hidden'
+          px='px-3'
+          onClick={toggleNavigation}>
           <MenuSvg openNavigation={openNavigation} />
         </Button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
