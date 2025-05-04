51a91c16f55bcdc30bd0cc8d42cff30c342e2cb8
Joy Mridha <joymridha939@gmail.com>
2025-05-05 01:35:25 +0530

    Feat: Update CORS configuration to include dynamic client origin from environment variable

--- server/server.js
+++ server/server.js
@@ -33,7 +33,12 @@ app.use(
 app.use(cookieParser())
 app.use(
 	cors({
-		origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+		origin: [
+			'http://localhost:5173',
+			'http://localhost:5174',
+			'http://localhost:5175',
+			process.env.CLIENT_ORIGIN_URL,
+		],
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
 		credentials: true,
 	})
